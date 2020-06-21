eefe2206504d507db018d99b22d35b3200cbf21a
democat3457 <cwongmath@gmail.com>
2020-06-20 22:08:45 -0500

    Initial forge mdk commit

---
+++ src/main/java/com/rebirthofthenight/rotntweaker/RotNTweaker.java
@@ -0,0 +1,31 @@
+package com.example.examplemod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = RotNTweaker.MODID, name = RotNTweaker.NAME, version = RotNTweaker.VERSION)
+public class RotNTweaker
+{
+    public static final String MODID = "rotntweaker";
+    public static final String NAME = "RotN Tweaker";
+    public static final String VERSION = "2.78-1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
