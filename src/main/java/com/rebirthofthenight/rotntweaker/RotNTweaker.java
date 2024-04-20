7ce1578a77bcccf50c2d52bba587e01da4c82a64
Colin Wong <cwongmath@gmail.com>
2024-04-20 05:44:17 -0500

    Bump version to 0.1.0

--- src/main/java/com/rebirthofthenight/rotntweaker/RotNTweaker.java
+++ src/main/java/com/rebirthofthenight/rotntweaker/RotNTweaker.java
@@ -19,7 +19,7 @@ public class RotNTweaker {
     // -Dlegacy.debugClassLoading=true -Dlegacy.debugClassLoadingSave=true
     public static final String MODID = "rotntweaker";
     public static final String NAME = "RotN Tweaker";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.1.0";
 
     public static Logger logger;
 
