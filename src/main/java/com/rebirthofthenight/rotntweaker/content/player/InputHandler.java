b7b242c002e9b1ba72a04a898bd5ed5bfdcbb628
Eve <phanta@phanta.pl>
2026-08-25 23:11:04 -0500

    Fix sidedness issues in webbed potion effect handling

--- src/main/java/com/rebirthofthenight/rotntweaker/content/player/InputHandler.java
+++ src/main/java/com/rebirthofthenight/rotntweaker/content/player/InputHandler.java
@@ -4,12 +4,11 @@ import com.rebirthofthenight.rotntweaker.RotNTweaker;
 import com.rebirthofthenight.rotntweaker.network.KeyPressMessage;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
-
-import java.util.UUID;
 
 public class InputHandler {
 
@@ -31,30 +30,34 @@ public class InputHandler {
 
         if (keyForward.isKeyDown() && lastPressedKey != Keyboard.KEY_W) {
             lastPressedKey = Keyboard.KEY_W;
-            sendKeyPressMessage(Keyboard.KEY_W, mc.player.getUniqueID());
+            sendKeyPressMessage(Keyboard.KEY_W);
             //System.out.println("Sent a packet");
         }
         else if (keyLeft.isKeyDown() && lastPressedKey != Keyboard.KEY_A) {
             lastPressedKey = Keyboard.KEY_A;
-            sendKeyPressMessage(Keyboard.KEY_A, mc.player.getUniqueID());
+            sendKeyPressMessage(Keyboard.KEY_A);
             //System.out.println("Sent a packet");
 
         }
         else if (keyBack.isKeyDown() && lastPressedKey != Keyboard.KEY_S) {
             lastPressedKey = Keyboard.KEY_S;
-            sendKeyPressMessage(Keyboard.KEY_S, mc.player.getUniqueID());
+            sendKeyPressMessage(Keyboard.KEY_S);
             //System.out.println("Sent a packet");
 
         }
         else if (keyRight.isKeyDown() && lastPressedKey != Keyboard.KEY_D) {
             lastPressedKey = Keyboard.KEY_D;
-            sendKeyPressMessage(Keyboard.KEY_D, mc.player.getUniqueID());
+            sendKeyPressMessage(Keyboard.KEY_D);
             //System.out.println("Sent a packet");
 
         }
     }
 
-    private void sendKeyPressMessage(int keyCode, UUID playerUUID) {
-        RotNTweaker.NETWORK.sendToServer(new KeyPressMessage(keyCode, playerUUID));
+    private void sendKeyPressMessage(int keyCode) {
+        // in principle, could send the key code to the server too, but it's trivial for a malicious client to fake it
+        // anyways, so there's not really a good reason to
+        RotNTweaker.NETWORK.sendToServer(new KeyPressMessage());
+        final NBTTagCompound data = Minecraft.getMinecraft().player.getEntityData();
+        data.setInteger("webbed_struggle", data.getInteger("webbed_struggle") + 1);
     }
 }
