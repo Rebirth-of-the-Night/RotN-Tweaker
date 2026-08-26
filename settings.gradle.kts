d6ffd6399ecaa4cc77d2e2225cb219348bb5100e
Eve <phanta@phanta.pl>
2026-08-25 20:42:56 -0500

    Modernize build system, clean up metadata, add JEI dependency

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+rootProject.name = "RotN-Tweaker"
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        // RetroFuturaGradle
+        maven {
+            name = "GTNH Maven"
+            url = uri("https://nexus.gtnewhorizons.com/repository/public/")
+            mavenContent {
+                includeGroupByRegex("com\\.gtnewhorizons\\..+")
+                includeGroup("com.gtnewhorizons")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    // If in a Nix shell, just use toolchains provided by Nix
+    if (System.getenv("IN_NIX_SHELL") == null) {
+        // Automatic toolchain provisioning
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+    }
+}
--- src/main/java/com/rebirthofthenight/rotntweaker/RotNTweaker.java
+++ src/main/java/com/rebirthofthenight/rotntweaker/RotNTweaker.java
@@ -49,13 +49,13 @@
     import java.util.List;
 
     @Mod.EventBusSubscriber
-    @Mod(modid = RotNTweaker.MODID, name = RotNTweaker.NAME, version = RotNTweaker.VERSION)
+    @Mod(modid = RotNTweaker.MODID, name = RotNTweaker.NAME, version = RotNTweaker.VERSION, useMetadata = true)
     public class RotNTweaker {
 
         // -Dlegacy.debugClassLoading=true -Dlegacy.debugClassLoadingSave=true
         public static final String MODID = "rotntweaker";
         public static final String NAME = "RotN Tweaker";
-        public static final String VERSION = "0.1.0";
+        public static final String VERSION = RotNTweakerConsts.VERSION;
 
         public static Logger logger;
         public static final SimpleNetworkWrapper NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
